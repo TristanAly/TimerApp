a4e7032ba3861e0f4e24be3ad806b93d1e7a844b
AlyTristan <tristan.aly@gmail.com>
2022-05-30 16:22:58 +0200

    Initial Commit

---
+++ Timer1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Timer1
+//
+//  Created by Tristan Aly on 30/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Timer1/Timer1App.swift
@@ -0,0 +1,17 @@
+//
+//  Timer1App.swift
+//  Timer1
+//
+//  Created by Tristan Aly on 30/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Timer1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
